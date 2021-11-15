5072b3859a62f8d8248a847a724bedca6f32c7c8
nicholas shawn freeman <nick_freeman65@yahoo.com>
2021-11-15 15:16:00 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,4 +1,60 @@
-/* Your Code Here */
+function createEmployeeRecord(employee) {
+    let record = {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return record
+} 
+
+function createEmployeeRecords(employees) {
+    return employees.map(records => createEmployeeRecord(records))
+}
+
+const createTimeInEvent = function (dateStamp) {
+    let [date, hours] = dateStamp.split(" ")
+    hours = parseInt(hours, 10)
+    
+    let updateTimeEmployee = {
+        type: 'TimeIn',
+        hour: hours,
+        date: date
+    }
+
+    this.timeInEvents.push(updateTimeEmployee)
+    return this
+}
+
+const createTimeOutEvent = function (dateStamp) {
+    let [date, hours] = dateStamp.split(" ")
+    hours = parseInt(hours, 10)
+    
+    let updateTimeEmployee = {
+        type: 'TimeOut',
+        hour: hours,
+        date: date
+    }
+
+    this.timeOutEvents.push(updateTimeEmployee)
+    return this
+}
+
+function hoursWorkedOnDate(hourDate) {
+    let inHours = this.timeInEvents.find(inEvent => inEvent.date === hourDate)
+
+    let outHours = this.timeOutEvents.find(outEvent => outEvent.date === hourDate)
+    let hoursWorked = (outHours.hour - inHours.hour) / 100
+    return hoursWorked
+}
+
+function wagesEarnedOnDate(payDate) {
+    return hoursWorkedOnDate.call(this, payDate) * this.payPerHour
+}
+
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +77,13 @@ const allWagesFor = function () {
     return payable
 }
 
+const findEmployeeByFirstName = function (array, Name) {
+    let findEmployee = array.find(record => record.firstName === Name)
+    return findEmployee
+}
+
+const calculatePayroll = function (array) {
+    return array.reduce((total, rec) => {
+        return total + allWagesFor.call(rec)
+    }, 0)
+}
